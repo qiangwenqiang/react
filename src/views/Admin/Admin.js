cdcd9b7b9da83fd46fd378cafe6a44d9ab38ecb7
DESKTOP-CF6BR1S\Wenqiang Zhang <17600311412@163.com>
2020-07-08 11:42:16 +0800

    编写页面修改调试接口

--- src/views/Admin/Admin.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react'
-export default class Aogin extends Component {
-    render() {
-        return (
-            <div>admin</div>
-        )
-    }
-}
--- src/views/Login/Login.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-import './Login.css'
-export default class Login extends Component {
-    render() {
-        return (
-            <div className='login'>login</div>
-        )
-    }
-}
